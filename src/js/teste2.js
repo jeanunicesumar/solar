dbe54a45b11f99716e3504bf5948146a394aca7d
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-10-07 13:35:52 -0300

    Adicionando leitor texto e pagina de venda

--- src/js/teste2.js
+++ src/js/teste2.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { InteractionManager } from 'three.interactive';
+
+import sunTexture from "../img/sun.jpg";
 
 function lerTexto(){
   var mensagem = new SpeechSynthesisUtterance();
@@ -27,11 +30,19 @@ function lerTexto(){
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
 document.body.appendChild(container);
+console.log(container);
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// container.appendChild(renderer.domElement);
+const planetDiv = document.getElementById('planet');
+console.log(planetDiv);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild(renderer.domElement);
+renderer.setSize(planetDiv.clientWidth, planetDiv.clientHeight); // Use o tamanho da div "planet"
+planetDiv.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
@@ -41,16 +52,27 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0.0, 0.0, 10.0);
+// const camera = new THREE.OrthographicCamera(
 
+// );
+camera.position.set(0,0,5);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
+orbit.update();
 const interactionManager = new InteractionManager(
   renderer,
   camera,
   renderer.domElement
 );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const backgroundGeometry = new THREE.PlaneGeometry(100, 100); // Tamanho do plano de fundo
+// const backgroundMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff }); // Azul, por exemplo
+// const backgroundPlane = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+// scene.add(backgroundPlane);
+
+const geometry = new THREE.BoxGeometry(1,1,1,);
 const material = new THREE.MeshBasicMaterial();
+
 
 const cube = new THREE.Mesh(geometry, material);
 cube.addEventListener('mouseover', (event) => {
@@ -70,9 +92,35 @@ cube.addEventListener('click', (event) => {
 scene.add(cube);
 interactionManager.add(cube);
 
+// const cube = new THREE.Mesh(geometry, material);
+// cube.addEventListener('mouseover', (event) => {
+//   console.log("Cheguei aqui")
+//   event.target.material.color.set(0xff0000);
+//   document.body.style.cursor = 'pointer';
+// });
+// cube.addEventListener('mouseout', (event) => {
+//   event.target.material.color.set(0xffffff);
+//   document.body.style.cursor = 'default';
+// });
+// cube.addEventListener('mousedown', (event) => {
+//   event.target.scale.set(1.1, 1.1, 1.1);
+// });
+// cube.addEventListener('click', (event) => {
+//   event.target.scale.set(1.0, 1.0, 1.0);
+// });
+
+// const z = document.createElement('div'); // Crie um elemento div para conter o cubo
+// z.style.width = '600px'; // Defina a largura desejada para o contêiner (ajuste conforme necessário)
+// z.style.height = '600px'; // Defina a altura desejada para o contêiner (ajuste conforme necessário)
+// z.style.position = 'relative'; // Defina a posição como relativa
+// document.body.appendChild(z); // Adicione o contêiner "z" ao corpo do documento
+
+// scene.add(cube);
+// interactionManager.add(cube);
+
 const animate = (time) => {
   requestAnimationFrame(animate);
-
+  cube.rotation.y+= 0.01;
   interactionManager.update();
 
   renderer.render(scene, camera);
